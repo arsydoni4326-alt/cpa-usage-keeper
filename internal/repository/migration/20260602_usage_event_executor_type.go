d2d8e17bec7f7cc97cb98ac09aaebde3af36f8d1
Will <wangxking@outlook.com>
2026-06-02 10:03:11 +0800

    Use GORM migrator for executor type column

--- internal/repository/migration/20260602_usage_event_executor_type.go
+++ internal/repository/migration/20260602_usage_event_executor_type.go
@@ -15,7 +15,7 @@ func addUsageEventExecutorTypeMigration(tx *gorm.DB) error {
 	if tx.Migrator().HasColumn(&entities.UsageEvent{}, "executor_type") {
 		return nil
 	}
-	if err := tx.Exec("ALTER TABLE usage_events ADD COLUMN executor_type TEXT NOT NULL DEFAULT ''").Error; err != nil {
+	if err := tx.Migrator().AddColumn(&entities.UsageEvent{}, "ExecutorType"); err != nil {
 		return fmt.Errorf("add usage_events.executor_type column: %w", err)
 	}
 	return nil
